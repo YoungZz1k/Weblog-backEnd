278dbbd25a4009defc8765dfa35bbd74b2198169
YoungZz1k <907175262@qq.com>
2023-09-26 11:12:01 +0800

    Spring Security 整合JWT 实现接口鉴权

--- weblog-springboot/weblog-module-jwt/src/main/java/com/youngzz1k/weblog/jwt/utils/JwtTokenHelper.java
+++ weblog-springboot/weblog-module-jwt/src/main/java/com/youngzz1k/weblog/jwt/utils/JwtTokenHelper.java
@@ -34,6 +34,12 @@ public class JwtTokenHelper implements InitializingBean {
     private JwtParser jwtParser;
 
     /**
+     * Token 失效时间（分钟）
+     */
+    @Value("${jwt.tokenExpireTime}")
+    private Long tokenExpireTime;
+
+    /**
      * 解码配置文件中配置的 Base 64 编码 key 为秘钥
      * @param base64Key
      */
@@ -62,10 +68,11 @@ public class JwtTokenHelper implements InitializingBean {
      */
     public String generateToken(String username) {
         LocalDateTime now = LocalDateTime.now();
-        // Token 一个小时后失效
-        LocalDateTime expireTime = now.plusHours(1);
+        // 设置 Token 失效时间
+        LocalDateTime expireTime = now.plusHours(tokenExpireTime);
 
-        return Jwts.builder().setSubject(username)
+        return Jwts.builder()
+                .setSubject(username)
                 .setIssuer(issuer)
                 .setIssuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
                 .setExpiration(Date.from(expireTime.atZone(ZoneId.systemDefault()).toInstant()))
@@ -106,4 +113,30 @@ public class JwtTokenHelper implements InitializingBean {
         String key = generateBase64Key();
         System.out.println("key: " + key);
     }
+
+    /**
+     * 校验 Token 是否可用
+     * @param token
+     * @return
+     */
+    public void validateToken(String token) {
+        jwtParser.parseClaimsJws(token);
+    }
+
+    /**
+     * 解析 Token 获取用户名
+     * @param token
+     * @return
+     */
+    public String getUsernameByToken(String token) {
+        try {
+            Claims claims = jwtParser.parseClaimsJws(token).getBody();
+            String username = claims.getSubject();
+            return username;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
