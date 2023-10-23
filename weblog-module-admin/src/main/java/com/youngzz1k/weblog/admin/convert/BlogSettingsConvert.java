faeab1bd09c6aea7c677ce6e6739848312ea70bc
YoungZz1k <907175262@qq.com>
2023-10-23 10:30:19 +0800

    2023/10/23 整合 Mapstruct : 简化属性映射

---
+++ weblog-module-admin/src/main/java/com/youngzz1k/weblog/admin/convert/BlogSettingsConvert.java
@@ -0,0 +1,22 @@
+package com.youngzz1k.weblog.admin.convert;
+
+
+import com.youngzz1k.weblog.admin.model.vo.blogsettings.UpdateBlogSettingsReqVO;
+import com.youngzz1k.weblog.common.domain.dos.BlogSettingsDO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface BlogSettingsConvert {
+    /**
+     * 初始化 convert 实例
+     */
+    BlogSettingsConvert INSTANCE = Mappers.getMapper(BlogSettingsConvert.class);
+
+    /**
+     * 将 VO 转化为 DO
+     * @param bean
+     * @return
+     */
+    BlogSettingsDO convertVO2DO(UpdateBlogSettingsReqVO bean);
+}
