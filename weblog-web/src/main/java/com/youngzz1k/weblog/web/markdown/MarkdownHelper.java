0331936b34ac91de5c14864dd15b5a2b46fc54ff
YoungZz1k <907175262@qq.com>
2024-03-13 22:06:22 +0800

    feat: 自定义 Markdown 解析：实现对超链接添加跳转 svg 图标

--- weblog-web/src/main/java/com/youngzz1k/weblog/web/markdown/MarkdownHelper.java
+++ weblog-web/src/main/java/com/youngzz1k/weblog/web/markdown/MarkdownHelper.java
@@ -1,7 +1,7 @@
 package com.youngzz1k.weblog.web.markdown;
 
-import com.youngzz1k.weblog.web.markdown.provider.NofollowLinkAttributeProvider;
 import com.youngzz1k.weblog.web.markdown.renderer.ImageNodeRenderer;
+import com.youngzz1k.weblog.web.markdown.renderer.LinkNodeRenderer;
 import org.commonmark.Extension;
 import org.commonmark.ext.gfm.tables.TablesExtension;
 import org.commonmark.ext.heading.anchor.HeadingAnchorExtension;
@@ -40,8 +40,8 @@ public class MarkdownHelper {
         PARSER = Parser.builder().extensions(extensions).build();
         HTML_RENDERER = HtmlRenderer.builder()
                 .extensions(extensions)
-                .attributeProviderFactory(context -> new NofollowLinkAttributeProvider())
-                .nodeRendererFactory(context -> new ImageNodeRenderer(context))
+                .nodeRendererFactory(context -> new ImageNodeRenderer(context)) // 自定义图片解析
+                .nodeRendererFactory(context -> new LinkNodeRenderer(context)) // 自定义超链接解析
                 .build();
     }
 
